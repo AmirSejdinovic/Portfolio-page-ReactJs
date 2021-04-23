47626d5bf6126202278f1388e17a4cf08e58a59c
Amir Sejdinovic <amir24V@yahoo.com>
2021-04-23 14:15:47 +0200

    Adding the hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection(){
+  return(
+    <div className="hero-container">
+      <video src="../assets/videos/video-1.mp4" autoPlay loop muted></video>
+      <h1>Modern react website</h1>
+      <p>This is a modern looking simple react site</p>
+      <div className="hero-btns">
+        <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">Get started</Button>
+
+        <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+          Watch <i className="fas fa-play-circle"></i>
+          </Button>
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroSection;
